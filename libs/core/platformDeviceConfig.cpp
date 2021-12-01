dc88a55cad7de0e5893d9d3dbf70da5003194121
Noel Chalmers <noel.chalmers@gmail.com>
2021-12-01 16:36:20 -0600

    [Platform] Programatically set the number of omp threads to use

--- libs/core/platformDeviceConfig.cpp
+++ libs/core/platformDeviceConfig.cpp
@@ -25,10 +25,29 @@ SOFTWARE.
 */
 
 #include "platform.hpp"
-// #include "omp.h"
+#include <omp.h>
 
 // OCCA build stuff
 void platform_t::DeviceConfig(){
+
+  //find out how many ranks and devices are on this system
+  char* hostnames = (char *) ::malloc(size*sizeof(char)*MPI_MAX_PROCESSOR_NAME);
+  char* hostname = hostnames+rank*MPI_MAX_PROCESSOR_NAME;
+
+  int namelen;
+  MPI_Get_processor_name(hostname,&namelen);
+
+  MPI_Allgather(MPI_IN_PLACE , MPI_MAX_PROCESSOR_NAME, MPI_CHAR,
+                hostnames, MPI_MAX_PROCESSOR_NAME, MPI_CHAR, MPI_COMM_WORLD);
+
+  int localRank = 0;
+  int localSize = 0;
+  for (int n=0; n<rank; n++){
+    if (!strcmp(hostname, hostnames+n*MPI_MAX_PROCESSOR_NAME)) localRank++;
+  }
+  for (int n=0; n<size; n++){
+    if (!strcmp(hostname, hostnames+n*MPI_MAX_PROCESSOR_NAME)) localSize++;
+  }
 
   int plat=0;
   int device_id=0;
@@ -63,24 +82,6 @@ void platform_t::DeviceConfig(){
     if (size==1) {
       settings.getSetting("DEVICE NUMBER",device_id);
     } else {
-      //find out how many ranks and devices are on this system
-      char* hostnames = (char *) ::malloc(size*sizeof(char)*MPI_MAX_PROCESSOR_NAME);
-      char* hostname = hostnames+rank*MPI_MAX_PROCESSOR_NAME;
-
-      int namelen;
-      MPI_Get_processor_name(hostname,&namelen);
-
-      MPI_Allgather(MPI_IN_PLACE , MPI_MAX_PROCESSOR_NAME, MPI_CHAR,
-                    hostnames, MPI_MAX_PROCESSOR_NAME, MPI_CHAR, MPI_COMM_WORLD);
-
-      int localRank = 0;
-      int localSize = 0;
-      for (int n=0; n<rank; n++){
-        if (!strcmp(hostname, hostnames+n*MPI_MAX_PROCESSOR_NAME)) localRank++;
-      }
-      for (int n=0; n<size; n++){
-        if (!strcmp(hostname, hostnames+n*MPI_MAX_PROCESSOR_NAME)) localSize++;
-      }
 
       device_id = localRank;
 
@@ -92,8 +93,6 @@ void platform_t::DeviceConfig(){
         HIPBONE_WARNING(ss.str());
         device_id = device_id%deviceCount;
       }
-      MPI_Barrier(MPI_COMM_WORLD);
-      free(hostnames);
     }
 
     // add device_id to setup string
@@ -101,14 +100,45 @@ void platform_t::DeviceConfig(){
     mode += ", device_id: " + std::to_string(device_id) + "}";
   }
 
-  //set number of omp threads to use
-  //int Ncores = sysconf(_SC_NPROCESSORS_ONLN);
-  //int Nthreads = Ncores/localSize;
-  // Nthreads = mymax(1,Nthreads/2);
-  // omp_set_num_threads(Nthreads);
+  /*set number of omp threads to use*/
+  /*Use lscpu to determine core and socket counts */
+  FILE *pipeCores   = popen("lscpu | grep \"Core(s) per socket\" | awk '{print $4}'", "r");
+  FILE *pipeSockets = popen("lscpu | grep \"Socket(s)\" | awk '{print $2}'", "r");
+  if (!pipeCores || !pipeSockets) {
+    HIPBONE_ABORT("popen() failed!");
+  }
+
+  std::array<char, 128> buffer;
+  if (!fgets(buffer.data(), buffer.size(), pipeCores)) { //read to end of line
+    HIPBONE_ABORT("Error reading core count")
+  }
+  int Ncores = std::stoi(buffer.data());
+
+  if (!fgets(buffer.data(), buffer.size(), pipeSockets)) { //read to end of line
+    HIPBONE_ABORT("Error reading core count")
+  }
+  int Nsockets = std::stoi(buffer.data());
+
+  pclose(pipeCores);
+  pclose(pipeSockets);
+
+  // int Ncores = omp_get_num_procs();
+  int NcoresPerNode = Ncores*Nsockets;
+  int Nthreads = NcoresPerNode/localSize;
+  if (Nthreads==0) {
+    stringstream ss;
+    ss << "Rank " << rank << " oversubscribing CPU on node \"" << hostname<< "\"";
+    HIPBONE_WARNING(ss.str());
+    Nthreads = 1;
+  }
+#if !defined(LIBP_DEBUG)
+  omp_set_num_threads(Nthreads);
+  // omp_set_num_threads(1);
 
   // if (settings.compareSetting("VERBOSE","TRUE"))
-  //   printf("Rank %d: Ncores = %d, Nthreads = %d, device_id = %d \n", rank, Ncores, Nthreads, device_id);
+    // printf("Rank %d: Nsockets = %d, NcoresPerSocket = %d, Nthreads = %d, device_id = %d \n",
+    //        rank, Nsockets, Ncores, Nthreads, device_id);
+#endif
 
   device.setup(mode);
 
@@ -128,4 +158,7 @@ void platform_t::DeviceConfig(){
     }
   }
   occa::env::setOccaCacheDir(occaCacheDir);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  free(hostnames);
 }
